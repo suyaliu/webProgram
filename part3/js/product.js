4b195356ee0ca880cc7746ff044670f7e35d88e9
suyaliu <81117888+suyaliu@users.noreply.github.com>
2021-11-08 18:54:41 -0500

    Add files via upload

---
+++ part3/js/product.js
@@ -0,0 +1,21 @@
+//set variable
+var quantity = 1;
+//var price =  document.querySelector('#total_price');
+//find quantitiy and total_price.
+var quantity_box = document.querySelector('#quantity');
+var total_amount = document.querySelector('#total_price');
+//declare the function add_one
+function add_one(){
+  quantity += 1;
+  quantity_box.value  = quantity;
+  total_amount.innerText  = quantity * price;
+}
+//declare the function minus_one
+function minus_one(){
+  quantity -= 1;
+  if (quantity < 1){
+    quantity = 1;
+  }
+  quantity_box.value  = quantity;
+  total_amount.innerText  = quantity * price;
+}
